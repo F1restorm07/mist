e81c56069b999c7192d393ef83313987ef847d1c
F1restorm <luke.n.gardner@icloud.com>
2024-03-25 21:40:21 -0600

    add http request parsing
    this also removes the need for allocation in the request struct

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,12 @@ use core::mem::MaybeUninit;
 pub use squid::{ Url, parse_url };
 mod status_code;
 mod header;
-pub mod parsers;
+ mod parsers;
 pub use status_code::StatusCode;
 pub use header::Header;
+pub use parsers::*;
 
+#[derive(Debug)]
 pub enum Method {
     /// request a representaion of the sepecified resource
     Get,
@@ -34,26 +36,66 @@ impl Default for Method {
     fn default() -> Self { Self::Get }
 }
 
+impl From<&str> for Method {
+    fn from(value: &str) -> Self {
+        match value {
+            "GET" => Self::Get,
+            "HEAD" => Self::Head,
+            "POST" => Self::Post,
+            "PATCH" => Self::Patch,
+            "PUT" => Self::Put,
+            "DELETE" => Self::Delete,
+            "TRACE" => Self::Trace,
+            "CONNECT" => Self::Connect,
+            "OPTIONS" => Self::Options,
+            _ => Self::Get,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum Version {
     /// HTTP/1.0
     V10,
     /// HTTP/1.1
     V11,
     /// HTTP/2.0
-    V2,
+    V20,
 }
 
 impl Default for Version {
     fn default() -> Self { Self::V11 }
 }
 
-pub struct Request<'r, B, const N: usize> {
+impl From<&str> for Version {
+    fn from(value: &str) -> Self {
+        match value {
+            "HTTP/1.0" => Self::V10,
+            "HTTP/1.1" => Self::V11,
+            "HTTP/2.0" => Self::V20,
+            _ => Self::V11,
+        }
+    }
+}
+
+pub struct Request<'r, 'h, B/* , const N: usize */> {
     method: Method,
-    target: Url<'r>, // TODO: add path and query type
+    target: Url<'r>,
     version: Version,
-    headers: [MaybeUninit<Header<'r>>; N],
-    __header_init_count: usize,
+    headers: &'h mut [Header<'r>],
     body: B,
+}
+
+impl<B: core::fmt::Debug> core::fmt::Debug for Request<'_, '_, B> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("Request")
+            .field("method", &self.method)
+            .field("url_target", &self.target)
+            .field("version", &self.version)
+            .field("headers", &self.headers)
+            .field("body", &self.body)
+            .finish()
+    }
 }
 
 pub struct Response<'r, B, const N: usize> {
@@ -64,44 +106,20 @@ pub struct Response<'r, B, const N: usize> {
     body: B
 }
 
-impl<'r, B, const N: usize> Request<'r, B, N> {
-    pub fn new(body: B) -> Self {
-        const UNINIT: MaybeUninit<Header<'_>> = MaybeUninit::uninit();
+impl<'r, 'h, B> Request<'r, 'h, B> {
+    pub fn new(body: B, headers: &'h mut [Header<'r>]) -> Self {
         Self {
             method: Method::default(),
             target: parse_url("/").unwrap(),
             version: Version::default(),
-            headers: [UNINIT; N],
-            __header_init_count: 0,
-            body,
-        }
-    }
-    pub fn new_with_headers(body: B, headers: [MaybeUninit<Header<'r>>; N]) -> Self {
-        Self {
-            method: Method::default(),
-            target: parse_url("/").unwrap(),
-            version: Version::default(),
-            __header_init_count: headers.len(),
             headers,
             body,
         }
     }
+
     pub fn method(&mut self, method: Method) { self.method = method; }
     pub fn url_target(&mut self, target: &'r str) { self.target = parse_url(target).unwrap(); }
     pub fn version(&mut self, version: Version) { self.version = version; }
-
-    pub fn add_header(&mut self, header: Header<'r>) {
-        assert!(self.__header_init_count < N);
-        self.headers[self.__header_init_count].write(header);
-        self.__header_init_count+=1;
-    }
-    pub fn add_headers<I>(&mut self, headers: impl IntoIterator<Item = Header<'r>>) {
-        assert!(self.__header_init_count < N);
-        for header in headers {
-            self.headers[self.__header_init_count].write(header);
-            self.__header_init_count+=1;
-        }
-    }
 }
 
 impl<'r, B, const N: usize> Response<'r, B, N> {
@@ -133,7 +151,7 @@ impl<'r, B, const N: usize> Response<'r, B, N> {
         self.headers[self.__header_init_count].write(header);
         self.__header_init_count+=1;
     }
-    pub fn add_headers<I>(&mut self, headers: impl IntoIterator<Item = Header<'r>>) {
+    pub fn add_headers(&mut self, headers: impl IntoIterator<Item = Header<'r>>) {
         assert!(self.__header_init_count < N);
         for header in headers {
             self.headers[self.__header_init_count].write(header);
@@ -141,3 +159,31 @@ impl<'r, B, const N: usize> Response<'r, B, N> {
         }
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     extern crate std;
+//     use super::*;
+//
+//     #[test]
+//     fn parse_full_request() {
+//         let request =
+//         "POST / HTTP/1.1\r\n\
+//         Host: localhost:8080\r\n\
+//         User-Agent: Mozilla/5.0 (Macintosh;...) Firefox/51.0\r\n\
+//         Accept: text/html,application/xhtml+xml,...,*/*;q=0.8\r\n\
+//         Accept-Language: en-US,en;q=0.5\r\n\
+//         Accept-Encoding: gzip,deflate\r\n\
+//         Connection: keep-alive\r\n\
+//         Upgrade-Insecure-Requests: 1\r\n\
+//         Content-Type: multipart/form-data; boundary=-12656974\r\n\
+//         Content-Length: 345\r\n\
+//         \r\n\
+//         -1265974\r\n\
+//         ";
+//         let mut header_buf = [header::EMPTY_HEADER; 9];
+//         let parsed_request = parse_request(request, &mut header_buf).unwrap();
+//         std::println!("{parsed_request:#?}");
+//         panic!();
+//     }
+// }
--- src/parsers.rs
+++ src/parsers.rs
@@ -1,8 +1,36 @@
-use crate::{ Header, Request, Response };
+use crate::{ Header, Request, Response, Method, Version };
 
-// pub fn parse_request(input: &str) -> Request {}
+#[derive(Debug)]
+pub struct HeaderParseError;
+#[derive(Debug)]
+pub struct RequestParseError;
+
+pub fn parse_request<'r, 'h>(input: &'r str, header_buf: &'h mut [Header<'r>]) -> Result<Request<'r, 'h, &'r str>, RequestParseError> {
+    let (header, body) = input.split_once("\r\n\r\n").unwrap_or(("", ""));
+    let mut header_lines = header.split("\r\n");
+    let mut status_line = header_lines.next().unwrap_or("").split_whitespace();
+    let method: Method = status_line.next().unwrap().into();
+    let url = status_line.next().unwrap();
+    let version: Version = status_line.next().unwrap().into();
+
+    for (header_idx, header) in header_lines.enumerate() {
+        if header_idx >= header_buf.len() { return Err(RequestParseError); }
+        header_buf[header_idx] = parse_header(header).unwrap();
+    }
+
+    let mut request = Request::new(body, header_buf);
+    request.method(method);
+    request.url_target(url);
+    request.version(version);
+    Ok(request)
+}
 // pub fn parse_response(input: &str) -> Request {}
-//
-// pub fn parse_header(input: &str) -> Header {
-//
-// }
+
+fn parse_header(input: &str) -> Result<Header, HeaderParseError> {
+    match input.split_once(": ") {
+        Some((name, value)) => Ok(Header::new(name.into(), value)),
+        None => Err(HeaderParseError)
+    }
+}
+
+// fn parse_headers(input: &str) -> []
